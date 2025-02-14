3cec8c87e1c21285d9616be7d7840fa8aafeda0a
abdulrafay23butt <abdulrafay23butt@gmail.com>
2025-02-14 19:07:49 +0500

    added subscriptions page

--- src/components/UI/Modal.tsx
+++ src/components/UI/Modal.tsx
@@ -8,11 +8,16 @@ interface ModalProps {
 }
 
 const Modal = ({ isOpen, onClose, children, buttonContent }: ModalProps) => {
-  if (!isOpen) return null
+  if (!isOpen) return null;
 
   return (
+<<<<<<< Updated upstream
     <div className="fixed inset-0 z-9999 flex items-center justify-center bg-black bg-opacity-50">
       <div className="bg-white w-full md:max-w-lg max-w-[350px] rounded-2xl md:p-6 p-4 shadow-lg relative">
+=======
+    <div className="fixed inset-0 z-[10000] flex items-center justify-center bg-black bg-opacity-50 p-4">
+      <div className="bg-white w-full md:max-w-lg max-w-[350px] rounded-2xl md:p-6 p-4 shadow-lg relative max-h-[90vh] overflow-y-auto">
+>>>>>>> Stashed changes
         {/* Close Button */}
         <button
           className="absolute top-4 right-4 text-gray-500 hover:text-black"
@@ -24,7 +29,7 @@ const Modal = ({ isOpen, onClose, children, buttonContent }: ModalProps) => {
         <div>{children}</div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Modal
+export default Modal;
