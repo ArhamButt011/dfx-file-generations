6555ed11ed0e44ddff0c188dc662265e022fea64
abdulrafay23butt <abdulrafay23butt@gmail.com>
2025-02-27 17:37:35 +0500

    added contact form

---
+++ src/app/Contact_Us/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ContactUS from '@/components/Home/ContactUS'
+import Navbar from '@/components/Home/navbar'
+import Footer from '@/components/Home/Footer'
+function page() {
+    return (
+        <div>
+            <Navbar />
+            <ContactUS />
+            <Footer />
+        </div>
+    )
+}
+
+export default page
