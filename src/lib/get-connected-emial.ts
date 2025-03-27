7cd7e3785a2d6422999120f39730ae2fcc101ac7
Arham utt011 <arhamhamidbutt@gmail.com>
2025-03-27 11:07:21 +0500

    Replace the landing page woth prelanding and delete middleware

--- src/lib/get-connected-emial.ts
+++ src/lib/get-connected-emial.ts
@@ -45,14 +45,13 @@ const GetConnectedEmail = async (email: string) => {
   <p style="color:#333333; font-size: 18px; font-weight: 600;">Hi there,</p>
   <div style="font-size: 16px;">
   <p style="margin-bottom: 30px;">Thank you for subscribing to Lumashape! You’re now on our exclusive list for the latest updates, special announcements, and early access to new features.</p>
-  <p style="margin-top: 30px;">If you have any questions or need assistance, please contact our support team at 
+  <p style="margin-top: 30px;">If you have any questions or need assistance, please contact our support team at
      <a href="mailto:sam.peterson@lumashape.com" style="color: #266CA8;">sam.peterson@lumashape.com</a></p>
- 
+
   </div>
-  <p style="margin-top: 60px;"><a href="https://www.lumashape.com" style="color: #000000; text-decoration: none;">www.lumashape.com</a> <span style="color: #000000;">  |  </span>     
+  <p style="margin-top: 60px;"><a href="https://www.lumashape.com" style="color: #000000; text-decoration: none;">www.lumashape.com</a> <span style="color: #000000;">  |  </span>
      <a href="mailto:sam.peterson@lumashape.com" style="color: #000000;">sam.peterson@lumashape.com</a></p>
 </div>
-
   `
 
   try {
