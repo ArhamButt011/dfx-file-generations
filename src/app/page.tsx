7f0e5d2a72da86b7ed1e859dda9be6f52613a618
mlb <mlb@mlbs-MacBook-Pro.local>
2025-06-02 17:20:30 +0500

    Add patent banner in landing page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,6 +6,7 @@ import Working from '@/components/Home/Working'
 import Pricing from '@/components/Home/Pricing'
 import FAQ from '@/components/Home/FAQ'
 import Footer from '@/components/Home/Footer'
+import Banner from '@/components/Home/Banner'
 // import ContactUS from '@/components/Home/ContactUS'
 // import { useAuth } from '@/context/AuthContext'
 
@@ -15,6 +16,7 @@ export default function Home() {
   return (
     <>
       <Navbar />
+      <Banner />
       <Hero />
       {/* <Sample /> */}
       <Working />
---
+++ src/components/Home/Banner.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Banner = () => {
+  return (
+    <div className="bg-[#C64949] md:text-[22px] text-white font-semibold text-center xl:mt-[90px] mt-18 md:py-4 py-3">
+      <p>A patent is pending for Lumashape</p>
+    </div>
+  );
+};
+
+export default Banner;
